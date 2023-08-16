e3f14265e6b617ce92ff8e41e40b0c30249e6501
Debora de Oliveira <deborapoh@gmail.com>
2023-08-16 16:18:06 -0300

    updates requests

--- src/app/api/users.ts
+++ src/app/api/users.ts
@@ -1,34 +1,29 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080'
+
 export const createUser = async (body: any) => {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+    const response = await fetch(`${BASE_URL}/users`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json', // Specify the content type as JSON
         },
         body,
-      });
-
-      console.log('RESPONSE', await response.json())
-
-      if (!response.ok) {
-          throw new Error('Network response was not ok');
-      }
+      })
 
       return await response.json();
   } catch (error: any) {
-    console.error('Error while posting data:', error.message);
+    console.error('Error while posting data:', error);
   }
 }
 
 export const getUsers = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`)
+  const response = await fetch(`${BASE_URL}/users`)
   return await response.json()
 }
 
 export const softDeleteUser = async (docId: string) => {
-  console.log('Soft delete', docId)
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/${docId}`, {
+    const response = await fetch(`${BASE_URL}/users/${docId}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
@@ -38,12 +33,8 @@ export const softDeleteUser = async (docId: string) => {
         })
       });
 
-      if (!response.ok) {
-          throw new Error('Network response was not ok');
-      }
-
       return await response.json();
   } catch (error: any) {
-    console.error('Error while posting data:', error.message);
+    console.error('Error while deleting data:', error);
   }
 }
